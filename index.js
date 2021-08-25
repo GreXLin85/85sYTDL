484080bcca15d78332e8fcc20af7a0537051444f
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2021-08-25 23:17:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-const { app, BrowserWindow, ipcMain } = require("electron")
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-let window
+const { app, BrowserWindow, ipcMain } = require("electron");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+let window;
 
 if (!fs.existsSync("Music")) {
-    fs.mkdirSync("Music")
+  fs.mkdirSync("Music");
 }
 
 app.on("ready", () => {
-    window = new BrowserWindow({ webPreferences: { nodeIntegration: true }, maximizable: false, minimizable: false, resizable: false })
+  window = new BrowserWindow({
+    webPreferences: { nodeIntegration: true },
+    maximizable: false,
+    minimizable: false,
+    resizable: false,
+  });
 
+  window.loadFile("./views/index.html");
+});
 
-    window.loadFile("./views/index.html")
-})
+ipcMain.on("links", async (event, arg) => {
+  let links = arg.split("\n");
+  let promises = [];
+  for (let i = 0; i < links.length; i++) {
+    promises.push(download(links[i], event));
+  }
+  let asd = await Promise.all(promises);
+  event.reply("linkreply", "ok");
+});
 
-ipcMain.on('link', (event, arg) => {
-    ytdl.getInfo(arg).then(info => {
-        let audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
-        let videoTitle = info.player_response.videoDetails.title
-        videoTitle = videoTitle.replace(/\/|\\|:|\*|\?|\"|\<|\>|\|/g, '');
-        let as = fs.createWriteStream("./Music/" + videoTitle + ".mp3")
-        ytdl.downloadFromInfo(info, { filter: 'audioonly' }).pipe(as)
-            .on("close", () => {
-                event.reply('linkreply', 'ok')
-            }).on("error", () => {
-                event.reply('linkreply', 'error')
-            }).on("drain", () => {
-                event.reply('progress', as.bytesWritten)
-            }).on("open", () => {
-                event.reply("started", audioFormats[0].contentLength)
-            })
-    })
-})
+function download(link, event) {
+  return new Promise((resolve, reject) => {
+    ytdl.getInfo(link).then((info) => {
+      let videoTitle = info.player_response.videoDetails.title;
+      videoTitle = videoTitle.replace(/\/|\\|:|\*|\?|\"|\<|\>|\|/g, "");
+      let as = fs.createWriteStream("./Music/" + videoTitle + ".mp3");
+      ytdl
+        .downloadFromInfo(info, { filter: "audioonly" })
+        .pipe(as)
+        .on("close", () => {
+          event.reply("downloadcount", "increase");
+          resolve("close");
+        })
+        .on("error", (err) => {
+          reject(new Error(err));
+        });
+    });
+  });
+}
